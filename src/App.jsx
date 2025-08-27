3d7c4caa5ad11023f3504306a18487bbe4da4d5b
ekacala <elizabethkacala@gmail.com>
2025-08-27 11:11:29 -0500

    Added in updated html

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,41 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+      </header>
+
+      <section id="intro">
+          <h1>Hello there!<br></br> I'm Elizabeth, a Software Engineer</h1>
+      </section>
+      <section id="skills">
+          <h2 id="skills_title">My Skills</h2>
+          <div id="skill_list">
+              <a class="skills_link" href="https://nodejs.org/en"><img src="public\nodejs-svgrepo-com.svg" id="nodejs_img" class="skills_img"></img></a>
+              <a class="skills_link" href="https://react.dev/"><img src="public\reactjs-fill-svgrepo-com.svg" id="reactjs_img" class="skills_img"></img></a>
+              <a class="skills_link" href="https://www.mysql.com/"><img src="public\mysql-svgrepo-com.svg" id="mysql_img" class="skills_img"></img></a>
+              <a class="skills_link" href="https://www.python.org/"><img src="public\python-svgrepo-com.svg" id="python_img" class="skills_img"></img></a>
+          </div>
+      </section>
+
+      <section id="projects">
+          <h2 id="projects_title">My Projects</h2>
+          <div id="project_list">
+              <a class="project_link" href="https://github.com/ekacala"><img src="public\github-svgrepo-com.svg" id="github_img" class="project_img"></img></a>
+          </div>
+      </section>
+      <section id="contact">
+          <h2 id="contact_title">Contact Me</h2>
+          <form id="email_form">
+              <label for="name" class="contact_label">Name: </label>
+              <input type="text" placeholder="Jane Doe" maxlength="70" id="name" name="name"></input>
+
+              <label for="email" class="contact_label">Email: </label>
+              <input type="email" placeholder="janedoe@gmail.com" maxlength="256" id="email" name="email"></input>
+
+              <label for="message" class="contact_label">Message: </label>
+              <input type="textarea" placeholder="Type your message here" maxlength="255" id="message" name="message"></input>
+          </form>
+      </section>
     </>
   )
 }
