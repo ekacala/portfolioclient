b56a948d80a4f63daefb9a981f8f59dd3b23f825
ekacala <elizabethkacala@gmail.com>
2025-09-24 14:47:31 -0500

    Updated project section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import mysqlLogo from '/mysql-svgrepo-com.svg'
 import pythonLogo from '/python-svgrepo-com.svg'
 import githubLogo from '../public/github-svgrepo-com.svg'
 import backlogReviewsMin from '../public/backlog_reviews_min.png'
+import puzzleTraversalMin from '../public/graph_traversal_puzzle_solver.png'
 import treactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -23,10 +24,10 @@ function App() {
       <section id="skills">
           <h2 id="skills_title">My Skills</h2>
           <div id="skill_list">
-              <a class="skills_link" href="https://nodejs.org/en"><img src={nodeLogo} id="nodejs_img" class="skills_img"></img></a>
-              <a class="skills_link" href="https://react.dev/"><img src={reactLogo} id="reactjs_img" class="skills_img"></img></a>
-              <a class="skills_link" href="https://www.mysql.com/"><img src={mysqlLogo} id="mysql_img" class="skills_img"></img></a>
-              <a class="skills_link" href="https://www.python.org/"><img src={pythonLogo} id="python_img" class="skills_img"></img></a>
+              <a class="skills_link" href="https://nodejs.org/en" target='_blank'><img src={nodeLogo} id="nodejs_img" class="skills_img"></img></a>
+              <a class="skills_link" href="https://react.dev/" target='_blank'><img src={reactLogo} id="reactjs_img" class="skills_img"></img></a>
+              <a class="skills_link" href="https://www.mysql.com/" target='_blank'><img src={mysqlLogo} id="mysql_img" class="skills_img"></img></a>
+              <a class="skills_link" href="https://www.python.org/" target='_blank'><img src={pythonLogo} id="python_img" class="skills_img"></img></a>
           </div>
       </section>
 
@@ -34,8 +35,12 @@ function App() {
           <h2 id="projects_title">My Projects</h2>
           <div id="project_list">
               <div class="project_card">
-                <a class="project_link" href="https://github.com/ekacala/backlog-reviews"><img src={backlogReviewsMin} id="backlog_img" class="project_img"></img></a>
+                <a class="project_link" href="https://github.com/ekacala/backlog-reviews" target='_blank'><img src={backlogReviewsMin} id="backlog_img" class="project_img"></img></a>
                 <p class="project_description">Frontend design for a gaming review website</p>
+              </div>
+              <div class="project_card">
+                <a class="project_link" href="https://github.com/ekacala/GraphTraversalPuzzleSolver" target='_blank'><img src={puzzleTraversalMin} id="puzzle_img" class="project_img"></img></a>
+                <p class="project_description">A graph traversal puzzle solver that uses a BFS approach</p>
               </div>
           </div>
       </section>
